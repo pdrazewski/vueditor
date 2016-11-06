ad042e43daf590b8776062ef2a1d66713aead0ec
defehub <drazewski.przemyslaw@gmail.com>
2016-11-07 00:01:30 +0100

    Add components

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 import Vue from 'vue'
 import store from './vuex/store'
 import Counter from './components/Counter.vue'
- 
+import Modes from './components/Modes.vue'
 
 const app = new Vue({
   el: '#app',
   store,
-  components: { Counter },
+  components: { Counter, Modes },
   template: `
     <div class="app">
       <counter></counter>
+      <modes></modes>
       <p>
     <button @click="increment">+</button>
     <button @click="decrement">-</button>
---
+++ components/Modes.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="enp-editor_actionbar"> 
+	    <ul>
+	        <li class="is-active"><a href="#" id="js-togglePreview"><i class="icon-buffer"></i></a></li>
+	        <li><a href="#" id="js-toggleHtmlView"><i class="icon-html5"></i></a></li>
+	        <li><a href="#" id="js-toggleHCSSView"><i class="icon-css3"></i></a></li>
+	        <li><a href="#" id="js-toggleJsView"><i class="icon-code"></i></a></li>
+	    </ul>
+	</div>
+</template>
+
+<script>
+module.exports = {
+  computed: {
+    count () {
+      console.log(this.$store)
+      return this.$store.state.count
+    }
+  }
+}
+</script>
+
