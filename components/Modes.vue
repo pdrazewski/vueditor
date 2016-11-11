ca7c499aa3b96e4258373d29d9c0ff1c08c52ec5
defehub <drazewski.przemyslaw@gmail.com>
2016-11-11 22:54:05 +0100

    Add

--- components/Modes.vue
+++ components/Modes.vue
@@ -2,12 +2,20 @@
 
 <template>
    <div class="enp-editor_actionbar"> 
-	    <ul style="width: auto">
+	    <ul style="width: auto; float: left">
 	        <li :class="{ 'is-active': mode == 'editor' }"><a  id="js-togglePreview" @click="changeMode('editor')">Editor</a></li>
 	        <li :class="{ 'is-active': mode == 'html' }"><a  id="js-toggleHtmlView"  @click="changeMode('html')">HTML</a></li>
 	        <li :class="{ 'is-active': mode == 'css' }"><a  id="js-toggleHCSSView"  @click="changeMode('css')">CSS</a></li>
 	        <li :class="{ 'is-active': mode == 'js' }"><a  id="js-toggleJsView" @click="changeMode('js')">JS</a></li>
 	        <li :class="{ 'is-active': mode == 'debug' }"><a  id="js-toggleDebugView" @click="changeMode('debug')">Debug</a></li>
+	        <li :class="{ 'is-active': mode == 'debug' }"><a  id="js-toggleDebugView" @click="changeMode('debug')">Tracking codes</a></li>
+	    </ul>
+	    <ul style="width: auto; float: right">
+	        <li ><a>Layers</a></li>
+	        <li ><a>Add +</a></li>
+	        <li ><a>History</a></li>
+	        <li ><a>Properties</a></li>
+	        <li ><a>Save...</a></li>
 	    </ul>
 	</div>
 </template>
